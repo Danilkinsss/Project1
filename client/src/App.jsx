5346bb19515e0ab8b8f4e43ed948420da19fdcc0
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2024-05-31 12:55:54 +0200

    Tomas time

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,70 @@
-import List from './components/List'
+import { useEffect, useState } from "react";
+import List from "./components/List";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [text, setText] = useState("");
+
+  const BASE_URL = "http://localhost:8080";
+  useEffect(() => {
+    const getPosts = () => {
+      fetch(`${BASE_URL}/posts`)
+        .then((res) => res.json())
+        .then((data) => setPosts(data));
+    };
+
+    setInterval(() => {
+      getPosts();
+    }, 1000);
+  }, []);
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      text,
+      author: "Milo",
+    };
+
+    fetch(`${BASE_URL}/posts`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    setText("");
+  };
+
   return (
-    <>
-      <div>
-        <header>
-          <h1>Home</h1>
-        </header>
-        <main>
-          <form>empty...</form>
-          <List />
-        </main>
-      </div>
-    </>
-  )
+    <div className="min-h-screen gap-4 flex flex-col bg-gray-100">
+      <header className="bg-white mx-20 py-4 gap-4 flex flex-col px-8">
+        <h1 className="text-2xl font-bold">Home</h1>
+        <form onSubmit={handleSubmit} className="flex gap-4">
+          <input
+            type="text"
+            value={text}
+            onChange={handleChange}
+            className="border w-full p-2 rounded-md"
+            placeholder="Write something..."
+          />
+          <button
+            type="submit"
+            className="bg-blue-600 text-white px-4 rounded-full"
+          >
+            Post
+          </button>
+        </form>
+      </header>
+      <main className="bg-white w-auto mx-20">
+        <List posts={posts} />
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- server/src/controllers/post.controller.js
+++ server/src/controllers/post.controller.js
@@ -1,3 +1,25 @@
-export function getPosts() {}
+import { posts } from "../models/data.js";
 
-export function makePost() {}
+export function getPosts(req, res) {
+  res.send(posts);
+}
+
+export function makePost(req, res) {
+  const { text, author } = req.body;
+
+  if (!text || text.length > 250) {
+    const error = new Error("Message must be between 1 and 250");
+    return res.status(400).json({ msg: error.message });
+  }
+
+  const newPost = {
+    id: posts.length,
+    text,
+    author,
+    postDate: new Date(),
+  };
+
+  posts.unshift(newPost);
+
+  res.status(201).json({ msg: "Posted successfully" });
+}
